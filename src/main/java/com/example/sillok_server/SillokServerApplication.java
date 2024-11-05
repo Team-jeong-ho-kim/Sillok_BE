6c95b70d91be526b637d7a7dae7965179e97134b
troy96475 <taesupark0504@gmail.com>
2024-11-05 19:14:51 +0900

    first commit

---
+++ src/main/java/com/example/sillok_server/SillokServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sillok_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SillokServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SillokServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sillok_server/SillokServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sillok_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SillokServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
