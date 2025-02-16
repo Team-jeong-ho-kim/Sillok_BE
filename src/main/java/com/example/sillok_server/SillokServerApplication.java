05799df0c70509b102e4be1738f7ae9ded8b6c5a
민수아 <Minsua0503@gmail.com>
2025-02-16 19:02:54 +0900

    (#2) chore :: @ConfigurationPropertiesScan 추가

--- src/main/java/com/example/sillok_server/SillokServerApplication.java
+++ src/main/java/com/example/sillok_server/SillokServerApplication.java
@@ -2,8 +2,10 @@ package com.example.sillok_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SillokServerApplication {
 
     public static void main(String[] args) {
