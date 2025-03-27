3a517c6f0e89d176105484278d0bdbcb74053dfe
민수아 <Minsua0503@gmail.com>
2025-03-27 11:10:48 +0900

    (#10) feat :: Major 추가

---
+++ src/main/java/com/example/sillok_server/domain/post/domain/Major.java
@@ -0,0 +1,14 @@
+package com.example.sillok_server.domain.post.domain;
+
+public enum Major {
+    BACKEND,
+    FRONTEND,
+    DESIGN,
+    AI,
+    IOS,
+    ANDROID,
+    FLUTTER,
+    EMBEDDED,
+    DEVOPS,
+    SECURITY
+}
