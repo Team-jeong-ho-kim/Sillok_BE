06e1156f5a7558cfb32873ca6456450576d85cf6
민수아 <Minsua0503@gmail.com>
2025-03-31 08:53:28 +0900

    (#10) feat :: 정규식 제약조건 추가

--- src/main/java/com/example/sillok_server/domain/post/presentation/dto/request/PostRequest.java
+++ src/main/java/com/example/sillok_server/domain/post/presentation/dto/request/PostRequest.java
@@ -3,6 +3,7 @@ package com.example.sillok_server.domain.post.presentation.dto.request;
 import com.example.sillok_server.domain.post.domain.Category;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record PostRequest(
@@ -12,6 +13,7 @@ public record PostRequest(
     String title,
 
     @NotBlank
+    @Pattern(regexp = "^(https?|ftp)://[^\\s/$.?#].[^\\s]*$", message = "유효한 URL 형식이어야 합니다")
     String link,
 
     @NotNull
