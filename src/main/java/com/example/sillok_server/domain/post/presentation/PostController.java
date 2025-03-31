a0292b85bbf54fecd20519b0badb8e4014400af6
민수아 <Minsua0503@gmail.com>
2025-03-31 22:54:20 +0900

    (#10) feat :: PostcontrollerDocs 추가

--- src/main/java/com/example/sillok_server/domain/post/presentation/PostController.java
+++ src/main/java/com/example/sillok_server/domain/post/presentation/PostController.java
@@ -2,22 +2,24 @@ package com.example.sillok_server.domain.post.presentation;
 
 import com.example.sillok_server.domain.post.presentation.dto.request.PostRequest;
 import com.example.sillok_server.domain.post.service.CreatePostService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Encoding;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/post")
-public class PostController {
+public class PostController implements PostControllerDocs {
 
     private final CreatePostService createPostService;
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public void createPost(@RequestPart(name = "request") @Valid PostRequest request,
                            @RequestPart(name = "image") MultipartFile image) {
