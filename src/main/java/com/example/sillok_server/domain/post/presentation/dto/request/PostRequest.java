ec5147f23a74e25f29491a8a4a0551e23bfe38ca
민수아 <Minsua0503@gmail.com>
2025-03-31 22:53:54 +0900

    (#10) feat :: 스웨거를 위한 @Schema 어노테이션 추가

--- src/main/java/com/example/sillok_server/domain/post/presentation/dto/request/PostRequest.java
+++ src/main/java/com/example/sillok_server/domain/post/presentation/dto/request/PostRequest.java
@@ -1,6 +1,7 @@
 package com.example.sillok_server.domain.post.presentation.dto.request;
 
 import com.example.sillok_server.domain.post.domain.Category;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -10,13 +11,16 @@ public record PostRequest(
 
     @NotBlank
     @Size(max = 30)
+    @Schema(description = "추천글 제목")
     String title,
 
     @NotBlank
+    @Schema(description = "추천할 글의 링크")
     @Pattern(regexp = "^(https?|ftp)://[^\\s/$.?#].[^\\s]*$", message = "유효한 URL 형식이어야 합니다")
     String link,
 
     @NotNull
+    @Schema(description = "추천글 카테고리")
     Category category
 
 ) {
