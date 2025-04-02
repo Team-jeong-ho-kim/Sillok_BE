7172ca39379545ab1052a0c61fdcceaf824db7bf
민수아 <Minsua0503@gmail.com>
2025-04-02 14:15:02 +0900

    (#12) refactor :: columnDefiniton 수정

--- src/main/java/com/example/sillok_server/domain/user/domain/User.java
+++ src/main/java/com/example/sillok_server/domain/user/domain/User.java
@@ -17,7 +17,7 @@ public class User {
     @Column(nullable = false, columnDefinition = "VARCHAR(10)")
     private String accountId;
 
-    @Column(nullable = false, columnDefinition = "CHAR(60)")
+    @Column(nullable = false, columnDefinition = "VARCHAR(20)")
     private String password;
 
 }
