f0e5e7ca220b082a5e2c632b6b43cc4d27131cf5
민수아 <Minsua0503@gmail.com>
2025-04-02 14:22:38 +0900

    (#12) feat :: AuthDetailsService 추가

---
+++ src/main/java/com/example/sillok_server/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.sillok_server.global.security.auth;
+
+import com.example.sillok_server.domain.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String accountId) throws UsernameNotFoundException {
+        return userRepository.findByAccountId(accountId)
+            .map(AuthDetails::new)
+            .orElseThrow(() -> new UsernameNotFoundException(accountId));
+    }
+
+}
