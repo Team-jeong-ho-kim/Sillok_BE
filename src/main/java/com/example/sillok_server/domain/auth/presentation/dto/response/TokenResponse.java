bd0b8426e8451662611a1b8d74affdd4ca014fd4
민수아 <Minsua0503@gmail.com>
2025-04-02 14:37:11 +0900

    (#12) feat :: @Schema 어노테이션 추가

--- src/main/java/com/example/sillok_server/domain/auth/presentation/dto/response/TokenResponse.java
+++ src/main/java/com/example/sillok_server/domain/auth/presentation/dto/response/TokenResponse.java
@@ -1,12 +1,17 @@
 package com.example.sillok_server.domain.auth.presentation.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.ZonedDateTime;
 
 @Builder
 public record TokenResponse(
+
+    @Schema(description = "액세스 토큰")
     String accessToken,
+
+    @Schema(description = "만료 기간")
     ZonedDateTime exp
 ) {
 }
