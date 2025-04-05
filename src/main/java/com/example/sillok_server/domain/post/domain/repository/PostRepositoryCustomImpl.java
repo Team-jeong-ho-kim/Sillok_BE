a884405ef32ad3cf47b66eb932a4f89dd6c6239d
민수아 <Minsua0503@gmail.com>
2025-04-05 14:19:54 +0900

    (#14) feat :: PostRepositoryCustomImpl 추가

---
+++ src/main/java/com/example/sillok_server/domain/post/domain/repository/PostRepositoryCustomImpl.java
@@ -0,0 +1,48 @@
+package com.example.sillok_server.domain.post.domain.repository;
+
+import com.example.sillok_server.domain.post.domain.QPost;
+import com.example.sillok_server.domain.post.domain.enums.Category;
+import com.example.sillok_server.domain.post.presentation.dto.response.PostResponse;
+import com.example.sillok_server.domain.post.presentation.dto.response.QPostResponse;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class PostRepositoryCustomImpl implements PostRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+    private final QPost post = QPost.post;
+
+    @Override
+    public List<PostResponse> findAllByIsApprovedTrue() {
+        return queryFactory
+            .select(new QPostResponse(
+                post.id,
+                post.title,
+                post.imageUrl,
+                post.category,
+                post.createdAt
+            ))
+            .from(post)
+            .where(post.isApproved.eq(true))
+            .fetch();
+    }
+
+    @Override
+    public List<PostResponse> findAllByCategoryAndIsApprovedTrue(Category category) {
+        return queryFactory
+            .select(new QPostResponse(
+                post.id,
+                post.title,
+                post.imageUrl,
+                post.category,
+                post.createdAt
+            ))
+            .from(post)
+            .where(post.category.eq(category).and(post.isApproved.eq(true)))
+            .fetch();
+    }
+
+}
