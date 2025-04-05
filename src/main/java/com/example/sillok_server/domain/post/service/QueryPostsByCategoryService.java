f7da33206a436bd92a36bf36a2d2e1cc19e0e675
민수아 <Minsua0503@gmail.com>
2025-04-05 14:21:06 +0900

    (#14) feat :: QueryPostsByCategoryService 추가

---
+++ src/main/java/com/example/sillok_server/domain/post/service/QueryPostsByCategoryService.java
@@ -0,0 +1,23 @@
+package com.example.sillok_server.domain.post.service;
+
+import com.example.sillok_server.domain.post.domain.enums.Category;
+import com.example.sillok_server.domain.post.domain.repository.PostRepository;
+import com.example.sillok_server.domain.post.presentation.dto.response.PostResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryPostsByCategoryService {
+
+    private final PostRepository postRepository;
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> execute(Category category) {
+        return postRepository.findAllByCategoryAndIsApprovedTrue(category);
+    }
+
+}
