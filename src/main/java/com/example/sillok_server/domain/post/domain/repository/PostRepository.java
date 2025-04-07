caf7bedc41275990ca1373da615b5b4a1675aec7
민수아 <Minsua0503@gmail.com>
2025-04-07 11:29:11 +0900

    (#14) refactor :: JpaRepository -> CrudRepository로 변경

--- src/main/java/com/example/sillok_server/domain/post/domain/repository/PostRepository.java
+++ src/main/java/com/example/sillok_server/domain/post/domain/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package com.example.sillok_server.domain.post.domain.repository;
 
 import com.example.sillok_server.domain.post.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
+public interface PostRepository extends CrudRepository<Post, Long>, PostRepositoryCustom {
 }
