82603e1ae386c4a8e8c41a3a3febea178f54d567
민수아 <Minsua0503@gmail.com>
2025-04-08 09:39:15 +0900

    (#18) refactor :: description 수정

--- src/main/java/com/example/sillok_server/domain/post/presentation/dto/response/PostResponse.java
+++ src/main/java/com/example/sillok_server/domain/post/presentation/dto/response/PostResponse.java
@@ -8,27 +8,31 @@ import java.time.LocalDate;
 
 public record PostResponse(
 
-    @Schema(description = "포스트 id")
+    @Schema(description = "추천글 id")
     Long id,
 
-    @Schema(description = "포스트 제목")
+    @Schema(description = "추천글 제목")
     String title,
 
-    @Schema(description = "포스트 이미지 url")
+    @Schema(description = "추천글 소개")
+    String introduction,
+
+    @Schema(description = "추천글 이미지 url")
     String imageUrl,
 
-    @Schema(description = "포스트 카테고리")
+    @Schema(description = "추천글 카테고리")
     Category category,
 
-    @Schema(description = "포스트 작성일")
+    @Schema(description = "추천글 작성일")
     LocalDate createdAt
 
 ) {
 
     @QueryProjection
-    public PostResponse(Long id, String title, String imageUrl, Category category, LocalDate createdAt) {
+    public PostResponse(Long id, String title, String introduction, String imageUrl, Category category, LocalDate createdAt) {
         this.id = id;
         this.title = title;
+        this.introduction = introduction;
         this.imageUrl = imageUrl;
         this.category = category;
         this.createdAt = createdAt;
