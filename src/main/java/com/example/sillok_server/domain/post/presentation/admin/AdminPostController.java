44f022f13d8786261db830f5887baf23e98876aa
민수아 <Minsua0503@gmail.com>
2025-04-10 17:19:44 +0900

    (#23) feat :: AdminDeletePostService 추가

--- src/main/java/com/example/sillok_server/domain/post/presentation/admin/AdminPostController.java
+++ src/main/java/com/example/sillok_server/domain/post/presentation/admin/AdminPostController.java
@@ -2,6 +2,7 @@ package com.example.sillok_server.domain.post.presentation.admin;
 
 import com.example.sillok_server.domain.post.presentation.dto.response.PostResponse;
 import com.example.sillok_server.domain.post.service.AdminApprovePostService;
+import com.example.sillok_server.domain.post.service.AdminDeletePostService;
 import com.example.sillok_server.domain.post.service.AdminQueryPostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,8 +17,9 @@ public class AdminPostController implements AdminPostControllerDocs {
 
     private final AdminQueryPostsService adminQueryPostsService;
     private final AdminApprovePostService adminApprovePostService;
+    private final AdminDeletePostService adminDeletePostService;
 
-    @GetMapping("/admin")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<PostResponse> adminQueryPosts() {
         return adminQueryPostsService.execute();
@@ -27,6 +29,12 @@ public class AdminPostController implements AdminPostControllerDocs {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void adminApprovePost(@PathVariable("post-id") Long postId) {
         adminApprovePostService.execute(postId);
+    }
+
+    @DeleteMapping("/{post-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void adminDeletePost(@PathVariable("post-id") Long postId) {
+        adminDeletePostService.execute(postId);
     }
 
 }
