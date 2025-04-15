ca11e8633ee191919fa1cea081b5dea22c913597
민수아 <Minsua0503@gmail.com>
2025-04-15 21:27:17 +0900

    delete :: 회원가입 api 삭제

--- src/main/java/com/example/sillok_server/domain/auth/presentation/AuthController.java
+++ src/main/java/com/example/sillok_server/domain/auth/presentation/AuthController.java
@@ -3,7 +3,6 @@ package com.example.sillok_server.domain.auth.presentation;
 import com.example.sillok_server.domain.auth.presentation.dto.request.AuthRequest;
 import com.example.sillok_server.domain.auth.presentation.dto.response.TokenResponse;
 import com.example.sillok_server.domain.auth.service.LoginService;
-import com.example.sillok_server.domain.auth.service.SignupService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,18 +14,11 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController implements AuthControllerDocs{
 
     private final LoginService loginService;
-    private final SignupService signupService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public TokenResponse login(@RequestBody @Valid AuthRequest request) {
         return loginService.execute(request);
-    }
-
-    @PostMapping("/sign-up")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void signup(@RequestBody @Valid AuthRequest request) {
-        signupService.execute(request);
     }
 
 }
