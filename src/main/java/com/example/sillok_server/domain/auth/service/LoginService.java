99f0256e0b7af725e1d7fe5675fb817c1120d95a
troy96475 <taesupark0504@gmail.com>
2025-04-15 13:12:19 +0900

    refactor :: passwordEncoder로 감싸기

--- src/main/java/com/example/sillok_server/domain/auth/service/LoginService.java
+++ src/main/java/com/example/sillok_server/domain/auth/service/LoginService.java
@@ -10,6 +10,7 @@ import com.example.sillok_server.global.error.exception.SillokException;
 import com.example.sillok_server.global.security.jwt.JwtProperties;
 import com.example.sillok_server.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,13 +24,14 @@ public class LoginService {
     private final UserRepository userRepository;
     private final JwtProperties jwtProperties;
     private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional(readOnly = true)
     public TokenResponse execute(AuthRequest request) {
         User user = userRepository.findByAccountId(request.accountId())
             .orElseThrow(() -> new SillokException(ErrorCode.USER_NOT_FOUND));
 
-        if (!user.getPassword().equals(request.password())) {
+        if (!passwordEncoder.matches(request.password(), user.getPassword())) {
             throw UserNotFoundException.EXCEPTION;
         }
 
