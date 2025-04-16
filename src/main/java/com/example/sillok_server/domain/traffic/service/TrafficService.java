e314fc36777ac81c32a4564fb634d242cbd8d98d
troy96475 <taesupark0504@gmail.com>
2025-04-16 22:51:42 +0900

    feat :: 방문자 조회 로직

---
+++ src/main/java/com/example/sillok_server/domain/traffic/service/TrafficService.java
@@ -0,0 +1,23 @@
+package com.example.sillok_server.domain.traffic.service;
+
+import com.example.sillok_server.domain.traffic.domain.Traffic;
+import com.example.sillok_server.domain.traffic.domain.repository.TrafficRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TrafficService {
+    private final TrafficRepository trafficRepository;
+
+    @Transactional
+    public void visit() {
+        Traffic traffic = trafficRepository.findById(1L)
+                .orElseGet(() -> {
+                    Traffic newTraffic = new Traffic();
+                    return trafficRepository.save(newTraffic);
+                });
+        traffic.visit();
+    }
+}
